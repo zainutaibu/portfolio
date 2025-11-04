7ddcf5e04b6dcde047ae77578bdedb2867f28f02
zainutaibu <zainutaibu@gmail.com>
2025-11-04 21:16:45 +0530

    chandes done

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
+
+
+
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  experimental: {
+    allowedDevOrigins: ["http://192.168.1.102:3000", "http://localhost:3000"],
+  },
 };
 
 export default nextConfig;
